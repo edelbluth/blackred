31e98124b4ac845c81ff4e963a772638e6a934c2
Juergen Edelbluth <edelbluth@users.noreply.github.com>
2016-01-20 05:37:56 +0100

    Year in Copyright Banners is 2016.

--- src/blackred/__init__.py
+++ src/blackred/__init__.py
@@ -1,5 +1,5 @@
 """
-Copyright 2015 Juergen Edelbluth
+Copyright 2016 Juergen Edelbluth
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
